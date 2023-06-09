c8e6d60334f457bdd62c2b7ed1873ad0964646a5
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-06-09 07:37:43 +0000

    Add error handling for auth - add abstract class for errors - add database connection error - add request validation error

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,13 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-errors';
+
 export const errorHandler = (
     err: Error,
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-    console.log('Something went wrong', err);
+    if (err instanceof CustomError) {
+        return res
+            .status(err.statusCode)
+            .send({ errors: err.serializeErrors() });
+    }
 
     res.status(400).send({
-        message: err.message,
+        errors: [{ message: 'Something went wrong' }],
     });
 };
