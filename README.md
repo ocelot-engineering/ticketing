66e3a7fc98c3ad99766580fcc9e36de4e59480f2
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-06-13 07:30:32 +0000

    Validate signin request - add check for JWT_KEY in start() - extract logic for request validation and put it into a validateRequest middleware - tranform mongodb return to no longer show password or some mongo metadata - add jwt key reference to k8s deployment

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ minikube start
 minikube addons enable ingress
 skaffold dev --trigger polling
 ```
+
+### Secret
+
+```
+kubectl create secret generic jwt-secret --from-literal=<eg. JWT_KEY=secret>
+kubectl get secrets
+```
--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -18,21 +18,33 @@ interface UserDoc extends mongoose.Document {
     password: string;
 }
 
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        requred: true,
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            requred: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    password: {
-        type: String,
-        required: true,
-    },
-});
+    {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.password;
+                delete ret.__v;
+            },
+        },
+    }
+);
 
 userSchema.pre('save', async function (done) {
     if (this.isModified('password')) {
         const hashed = await Password.toHash(this.get('password'));
-        this.set('password', hashed)
+        this.set('password', hashed);
     }
 });
 
