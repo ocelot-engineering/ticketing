0d2c62fb1678f0e67921b0a20d0731a9b05102a5
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-06-19 07:58:12 +0000

    Make landing page call auth service to get current user

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,23 @@
-const Landing = () => {
-    return <h1>Landing</h1>;
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
+    return <h1>Landing Page</h1>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = async ({ req }) => {
+    if (typeof window === 'undefined') {
+        // executed on server
+        const { data } = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            { headers: req.headers }
+        );
+        return data;
+    } else {
+        // executed on browser
+        const { data } = await axios.get('/api/users/currentuser');
+        return data;
+    }
+};
+
+export default LandingPage;
