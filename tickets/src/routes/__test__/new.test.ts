95abcd53ee1eb135f8fc2784b0ac6e9cd87efde2
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-06-26 07:54:15 +0000

    Add placeholder tests for creating new tickets

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,9 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { response } from 'express';
+
+it('has a route handler listing to /api/tickets for post requests', async () => {});
+it('can only be accessed if the user is signed in', async () => {});
+it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid price is provided', async () => {});
+it('creates a ticket with valid inputs', async () => {});
