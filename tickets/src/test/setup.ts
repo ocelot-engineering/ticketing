7bfc2d652e230a252f7bc60b7d421b6ab8f5d710
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-07-01 16:20:02 +0000

    Add update and index tickets routes - add tests for update and index tickets routes - update ingress service to include tickets routes

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -35,7 +35,7 @@ afterAll(async () => {
 global.signin = () => {
     // Build a JWT payload { id, email }
     const payload = {
-        id: '12345678',
+        id: new mongoose.Types.ObjectId().toHexString(),
         email: 'test@test.com',
     };
 
