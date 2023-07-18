53c963e2df41d3a347cf5bf33f5ea96c0c05f9cd
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-07-18 07:53:47 +0000

    Add subscription options to nats-test lisenter, including manual acknowledgement - add random string to client id for nats-test listener so multiple listeners can be active - add queue group for orders service

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,31 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
-const client = nats.connect('ticketing', '123', {
+const client = nats.connect('ticketing', randomBytes(4).toString('hex'), {
     url: 'http://localhost:4222',
 });
 
 client.on('connect', () => {
     console.log('Listener connected to NATS');
 
-    const subscription = client.subscribe('ticket:created');
+    const options = client.subscriptionOptions().setManualAckMode(true);
+    const subscription = client.subscribe(
+        'ticket:created',
+        'orders-service-queue-group',
+        options
+    );
 
-    subscription.on('message', (msg) => {
-        console.log('Message recieved');
+    subscription.on('message', (msg: Message) => {
+        const data = msg.getData();
+
+        if (typeof data === 'string') {
+            console.log(
+                `Recieved event #${msg.getSequence()}, with data: ${data}`
+            );
+        }
+
+        msg.ack();
     });
 });
