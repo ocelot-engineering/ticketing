2f8fb63f6b8484b8ce4300da021e751fce99b02d
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-07-26 07:24:58 +0000

    Add env variables for tickets service - fix update ticket tests that had incorrect http methods

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -15,7 +15,7 @@ it('returns a 404 if the provided id does not exist', async () => {
 it('returns a 401 if the user is not authenticated', async () => {
     const id = new mongoose.Types.ObjectId().toHexString();
     await request(app)
-        .put(`/api/tickets${id}`)
+        .put(`/api/tickets/${id}`)
         .send({ title: 'title', price: 20 })
         .expect(401);
 });
@@ -27,7 +27,7 @@ it('returns a 401 if the user does not own the ticket', async () => {
         .send({ title: 'title', price: 20 });
 
     await request(app)
-        .post(`/api/tickets/${response.body.id}`)
+        .put(`/api/tickets/${response.body.id}`)
         .set('Cookie', global.signin())
         .send({ title: 'new title', price: 99 })
         .expect(401);
@@ -41,13 +41,13 @@ it('returns a 400 if the user provides an invalid title or price', async () => {
         .send({ title: 'title', price: 20 });
 
     await request(app)
-        .post(`/api/tickets/${response.body.id}`)
+        .put(`/api/tickets/${response.body.id}`)
         .set('Cookie', cookie)
         .send({ title: '', price: 20 })
         .expect(400);
 
     await request(app)
-        .post(`/api/tickets/${response.body.id}`)
+        .put(`/api/tickets/${response.body.id}`)
         .set('Cookie', cookie)
         .send({ title: 'new title', price: -10 })
         .expect(400);
@@ -61,7 +61,7 @@ it('updates the ticket provided valid inputs', async () => {
         .send({ title: 'title', price: 20 });
 
     await request(app)
-        .post(`/api/tickets/${response.body.id}`)
+        .put(`/api/tickets/${response.body.id}`)
         .set('Cookie', cookie)
         .send({ title: 'new title', price: 100 })
         .expect(200);
@@ -82,7 +82,7 @@ it('publishes an event', async () => {
         .send({ title: 'title', price: 20 });
 
     await request(app)
-        .post(`/api/tickets/${response.body.id}`)
+        .put(`/api/tickets/${response.body.id}`)
         .set('Cookie', cookie)
         .send({ title: 'new title', price: 100 })
         .expect(200);
