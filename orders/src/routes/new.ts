d6ae139184785985b795a83e0319e5662743e600
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-07-30 18:17:38 +0000

    Map out new order request steps

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@wizard4571/common';
+import {
+    BadRequestError,
+    NotFoundError,
+    OrderStatus,
+    requireAuth,
+    validateRequest,
+} from '@wizard4571/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -17,6 +25,33 @@ router.post(
     ],
     validateRequest,
     async (req: Request, res: Response) => {
+        const { ticketId } = req.body;
+
+        // Find the ticket the user is trying to order in the db
+        const ticket = await Ticket.findById(ticketId);
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        // Make sure that the this ticket is not already reserved
+        const existingOrder = await Order.findOne({
+            ticket: ticket,
+            status: {
+                $in: [
+                    OrderStatus.Created,
+                    OrderStatus.AwaitingPayment,
+                    OrderStatus.Complete,
+                ],
+            },
+        });
+        if (existingOrder) {
+            throw new BadRequestError('Ticket is already reservered');
+        }
+        // Caclualte the expiration date for this order
+
+        // Build the order and save it to the db
+
+        // Publish an event saying that an order is created
         res.send({});
     }
 );
