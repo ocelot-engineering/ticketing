e099587fe1e2139d8dbc50092fb7960180ea6f8e
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-08-01 07:22:47 +0000

    Add order index route handler - add tests for order index route

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,57 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+const buildTicket = async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20,
+    });
+    await ticket.save();
+
+    return ticket;
+};
+
+it('fetches oders for a particular user', async () => {
+    // create three tickets
+    const ticket1 = await buildTicket();
+    const ticket2 = await buildTicket();
+    const ticket3 = await buildTicket();
+
+    const user1 = global.signin();
+    const user2 = global.signin();
+
+    // create one order as user #1
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', user1)
+        .send({ ticketId: ticket1.id })
+        .expect(201);
+
+    // create two orders as user #2
+    const { body: order1 } = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user2)
+        .send({ ticketId: ticket2.id })
+        .expect(201);
+
+    const { body: order2 } = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user2)
+        .send({ ticketId: ticket3.id })
+        .expect(201);
+
+    // make request to get orders for user #2
+    const response = await request(app)
+        .get('/api/orders')
+        .set('Cookie', user2)
+        .expect(200);
+
+    // make sure we only got the orders for user #2
+    expect(response.body.length).toEqual(2);
+    expect(response.body[0].id).toEqual(order1.id);
+    expect(response.body[1].id).toEqual(order2.id);
+    expect(response.body[0].ticket.id).toEqual(ticket2.id);
+    expect(response.body[1].ticket.id).toEqual(ticket3.id);
+});
