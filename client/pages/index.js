122b2e5567a52d73737c77d1efca6f97f0bb3adb
ocelot-engineering <19709369+ocelot-engineering@users.noreply.github.com>
2023-09-10 14:51:26 +0000

    Add new ticket page to client - add ticket list to landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,33 @@
-import axios from 'axios';
-import { buildClient } from '../api/build-client';
+const LandingPage = ({ currentUser, tickets }) => {
+    const ticketList = tickets.map((ticket) => {
+        return (
+            <tr key={ticket.id}>
+                <td>{ticket.title}</td>
+                <td>{ticket.price}</td>
+            </tr>
+        );
+    });
 
-const LandingPage = ({ currentUser }) => {
     return (
-        <h1> {currentUser ? 'You are signed in' : 'You are NOT signed in'}</h1>
+        <div>
+            <h1>Tickets</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>{ticketList}</tbody>
+            </table>
+        </div>
     );
 };
 
-LandingPage.getInitialProps = async (context) => {
-    const client = buildClient(context);
-    const { data } = await client.get('/api/users/currentuser');
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+    const { data } = await client.get('/api/tickets');
 
-    return data;
+    return { tickets: data };
 };
 
 export default LandingPage;
