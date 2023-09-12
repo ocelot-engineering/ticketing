c87cc7e1d5756514a281cffd9de761976e90ab7c
ocelot-engineering <19709369+ocelot-engineering@users.noreply.github.com>
2023-09-12 08:08:16 +0000

    Add ticket purchase button on selected ticket page - add order page with timer until order expires

---
+++ client/pages/orders/[orderid].js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+const OrderShow = () => {
+    const [timeLeft, setTimeLeft] = useState('');
+
+    useEffect(() => {
+        const findTimeLeft = () => {
+            const msLeft = new Date(order.expiresAt) - new Date();
+            setTimeLeft(Math.round(msLeft / 1000));
+        };
+        const timerId = setInterval(findTimeLeft, 1000);
+
+        return () => {
+            clearInterval(timerId);
+        };
+    }, [order]);
+
+    findTimeLeft();
+    return <div>Time left to pay: {timeLeft} seconds</div>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+    const { orderId } = context.query;
+    const { data } = await client.get(`/api/orders/${orderId}`);
+
+    return { order: data };
+};
+
+export default OrderShow;
