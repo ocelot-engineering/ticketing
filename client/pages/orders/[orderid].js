8239df7e6a527d8c8fa0ee3d94a5d51a5d44657c
ocelot-engineering <19709369+ocelot-engineering@users.noreply.github.com>
2023-09-14 08:08:07 +0000

    Add stripe checkout to order page

--- client/pages/orders/[orderid].js
+++ client/pages/orders/[orderid].js
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 const OrderShow = () => {
-    const [timeLeft, setTimeLeft] = useState('');
+    const [timeLeft, setTimeLeft] = useState(0);
 
     useEffect(() => {
         const findTimeLeft = () => {
             const msLeft = new Date(order.expiresAt) - new Date();
             setTimeLeft(Math.round(msLeft / 1000));
         };
+
+        findTimeLeft();
         const timerId = setInterval(findTimeLeft, 1000);
 
         return () => {
@@ -15,8 +18,21 @@ const OrderShow = () => {
         };
     }, [order]);
 
-    findTimeLeft();
-    return <div>Time left to pay: {timeLeft} seconds</div>;
+    if (timeLeft < 0) {
+        return <div>Order Expired</div>;
+    }
+
+    return (
+        <div>
+            Time left to pay: {timeLeft} seconds
+            <StripeCheckout
+                token={(token) => console.log(token)}
+                stripeKey="pk_test_51NiB3KCjXeqtVbHpvOeDOMWxMe8I58nvuzW2YSOMMIcUNd0YiDP5z9gXJDg1IHY58vgL8cLjtOLSNftMEeNwuCgI00avXX340w"
+                amount={order.ticket.price * 100}
+                email={currentUser.email}
+            />
+        </div>
+    );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
